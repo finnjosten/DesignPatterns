5efff442c3734610b61e41c342945c3e3e4292d9
Finn Josten <finn.r.josten@proton.me>
2025-09-11 13:41:48 +0200

    Observer pattern

---
+++ ObserverPattern/Displays/StatisticsDisplay.cs
@@ -0,0 +1,52 @@
+﻿using ObserverPattern.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverPattern.Displays
+{
+    internal class StatisticsDisplay : Observer, DisplayElement
+    {
+        private float temperature;
+        private float sumTemperature = 0;
+        private float maxTemp = 0;
+        private float minTemp = 0;
+        private int countUpdated = 0;
+
+        private Subject weatherData;
+        
+
+        public StatisticsDisplay(Subject weatherData)
+        {
+            this.weatherData = weatherData;
+            weatherData.RegisterObserver(this);
+        }
+        public void Update(float temp, float humidity, float pressure)
+        {
+            this.temperature = temp;
+            this.sumTemperature += temp;
+            this.countUpdated++;
+
+            if (countUpdated == 1)
+            {
+                maxTemp = temp;
+                minTemp = temp;
+            }
+            else
+            {
+                if (temp > maxTemp) maxTemp = temp;
+                if (temp < minTemp) minTemp = temp;
+            }
+
+            Display();
+        }
+
+        public void Display()
+        {
+            float averageTemp = sumTemperature / countUpdated;
+            Console.WriteLine($"Avg/Max/Min temperature = {averageTemp}/{maxTemp}/{minTemp}");
+        }
+    }
+}
---
+++ ObserverPattern/Program.cs
@@ -0,0 +1,21 @@
+﻿using ObserverPattern.Displays;
+
+namespace ObserverPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WeatherData weatherData = new WeatherData();
+
+            CurrentConditionDisplay currentDisplay = new CurrentConditionDisplay(weatherData);
+            StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+            ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+
+
+            weatherData.SetMeasurements(28, 65, 30.4f);
+            weatherData.SetMeasurements(29, 70, 29.2f);
+            weatherData.SetMeasurements(30, 90, 29.2f); 
+        }
+    }
+}

8ce0ce0b3f70dfb69e0e63d48b309db16c64bb1e
Finn Josten <finn.r.josten@proton.me>
2025-10-02 13:48:58 +0200

    Singleton Pattern

---
+++ SingletonPattern/ChocolateBoiler.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    internal class ChocolateBoiler
+    {
+        private static ChocolateBoiler uniqueInstance = new ChocolateBoiler();
+
+        private bool empty;
+        private bool boiled;
+
+        public bool IsEmpty { get { return this.empty; } }
+        public bool IsBoiled { get { return this.boiled; } }
+
+        // This code is only started when the boiler is empty
+        private ChocolateBoiler()
+        {
+            empty = true;
+            boiled = false;
+        }
+        
+        public static ChocolateBoiler getInstance()
+        {
+            return uniqueInstance;
+        }
+
+
+        // To fill the boiler it must be empty and once it is full, we set the empty and boiled flag
+        public void fill()
+        {
+            if (empty)
+            {
+                empty = false;
+                boiled = false;
+            }
+        }
+        // To drain the boiler, it must be full (non empty) and also boiled.
+        // Once it is drained we set empty back to true
+        public void drain()
+        {
+            if(!empty && boiled)
+            {
+                empty = true;
+            }
+        }
+        // To boil the mixture, the boiler has to be full and not already boiled.
+        // Once it is boiled we set the boiled flag to true
+        public void boil()
+        {
+            if(!empty && !boiled)
+            {
+                boiled = true;
+            }
+        }
+    }
+}
